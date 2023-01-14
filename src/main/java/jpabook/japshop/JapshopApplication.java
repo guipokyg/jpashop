f0ffa019abb63f7bd821004478724d45a4ffdbdc
guipokyg <guipokyg77@gmail.com>
2023-01-14 10:19:53 +0900

    jpa 커밋

---
+++ src/main/java/jpabook/japshop/JapshopApplication.java
@@ -0,0 +1,19 @@
+package jpabook.japshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapshopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+
+		System.out.println("data = " + data);
+
+		SpringApplication.run(JapshopApplication.class, args);
+	}
+
+}
