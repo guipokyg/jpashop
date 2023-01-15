ce17bd972082c5fc6a9edc9a7696a70e75bec215
guipokyg <guipokyg77@gmail.com>
2023-01-15 12:32:19 +0900

    jpa entity 설계

---
+++ src/main/java/jpabook/japshop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.japshop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id@GeneratedValue
+    @Column(name="delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order oder;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private  DeliveryStatus status;
+}
